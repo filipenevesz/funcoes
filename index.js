80da0aa70ff394e9c1177ed2652b5289dbb0ce6d
Filipe <fedzn11@gmail.com>
2022-01-14 14:52:20 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,14 @@
+/* function soma(x = 1,y = 5) {
+    const resultado = x * y;
+    return resultado;
+}
+
+const resultado = soma(10);
+console.log(resultado) */
+
+const raiz = n => n ** 0.5;
+
+console.log(raiz(9))
+console.log(raiz(25))
+console.log(raiz(36))
+console.log(raiz(93))
